239f286e54bef64d5e6b7258bc411675b9a476e2
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2021-01-30 22:11:10 +0100

    Add current time printout

--- examples/timers/timers.ino
+++ examples/timers/timers.ino
@@ -4,15 +4,18 @@
 
 #include "MySystem.h"
 
-#include <sys/time.h>    //  This header is needed to simulate a time sync. Not needed if time is only read
+#include <sys/time.h>    //  This header is needed to simulate a time sync. Not needed for time actions
 
 using namespace ds;
+
+time_t old_time, new_time;
 
 void setup() {
   System::begin();
 
   // Set system time to some value
-  time_t new_time = 1000000000;                       // Some date around 2001
+  new_time = 1000000000;                              // Some date around 2001
+  old_time = new_time;
   struct timeval tv_new_time = {new_time, 0};
   settimeofday(&tv_new_time, NULL);
   delay(100);                                         // Allow new time to propagate
@@ -25,10 +28,14 @@ void setup() {
     System::timers.push_front(my_timer);
     System::log->printf(TIMED("Timer %hhu set to fire every day at %02d:%02d\n"), n, tinfo->tm_hour, tinfo->tm_min);    
   }
-
-  System::log->println("Now sit back and wait...");
 }
 
 void loop() {
+  new_time = System::getTime();
+  if (new_time != old_time) {
+    old_time = new_time;
+    struct tm *tinfo = localtime(&new_time);
+    System::log->printf(TIMED("Current time: %02d:%02d\n"), tinfo->tm_hour, tinfo->tm_min);
+  }
   System::update();
 }
