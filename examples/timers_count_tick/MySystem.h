4afdf10819cc3a78379a109b53a2614461e79d89
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2021-03-05 22:49:35 +0100

    No system time needed for Ticker example

--- examples/timers_count_tick/MySystem.h
+++ examples/timers_count_tick/MySystem.h
@@ -1,7 +1,6 @@
 #ifndef _DS_SYSTEM_H_
 
 #define DS_CAP_SYS_LOG_HW        // Enable syslog on hardware serial line
-#define DS_CAP_SYS_TIME          // Enable system time
 #define DS_CAP_TIMERS_COUNT_TICK // Enable countdown timers via ticker
 
 #include "System.h"         // System global definitions
