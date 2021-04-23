b4fce01fa051e661d8cfdff6d5efc3a8aee52626
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2021-04-23 23:13:25 +0200

    Remove statically defined timers in favor of user-defined ones

--- examples/web_timers/web_timers.ino
+++ examples/web_timers/web_timers.ino
@@ -10,29 +10,44 @@ using namespace ds;
 const char *System::wifi_ssid = "mySSID";             // Network SSID
 const char *System::wifi_pass = "myPassword";         // Network password
 
+// Timer handler
+void myTimerHandler(const TimerAbsolute* timer) {
+  static auto lamp_on = false;
+  if (timer->getLabel() == "lamp on") {
+
+    // Turn the lamp on here
+    // ...
+    lamp_on = true;
+    System::log->println("Lamp is ON");
+  } else
+  if (timer->getLabel() == "lamp off") {
+
+    // Turn the lamp off here
+    // ...
+    lamp_on = false;
+    System::log->println("Lamp is OFF");
+  } else
+  if (timer->getLabel() == "lamp toggle") {
+
+    // Toggle the lamp here
+    // ...
+    lamp_on = !lamp_on;
+    System::log->printf("Lamp is %s\n", lamp_on ? "ON" : "OFF");
+  }
+}
+void (*System::timerHandler)(const TimerAbsolute*) = myTimerHandler;  // Install the handler
+
 void setup() {
   System::begin();
 
   // Define some timer actions
   System::timer_actions.push_front("lamp off");
   System::timer_actions.push_front("lamp on");
-  System::timer_actions.push_front("\\uD83D\\uDCA1 lamp toggle");  // Example of action including an icon (must be UTF-16 encoded for use with JS)
+  System::timer_actions.push_front("lamp toggle");
+//  System::timer_actions.push_front("\\uD83D\\uDCA1 lamp toggle");  // Example of action including an icon (must be UTF-16 encoded for use with JS)
   System::timer_actions.reverse();     // Optional; call this to keep the order as written
 
-  // Add a timer at 07:50 every Sunday to turn the lamp off
-  auto my_timer = new TimerAbsolute("lamp off", 7, 50, 0, TIMER_DOW_SUNDAY);
-  System::timers.push_front(my_timer);
-
-  // Add a timer at quarter to sunset to turn the lamp on
-  my_timer = new TimerSolar(TIMER_SUNSET, "lamp on", -15);
-  System::timers.push_front(my_timer);
-
-  // Add a timer firing every hour offset from midnight by 30 mins. Disable it by default
-  my_timer = new TimerCountdownAbs("\\uD83D\\uDCA1 lamp toggle", 60, 30, TIMER_DOW_ANY, 0);
-  System::timers.push_front(my_timer);
-
-  System::timers.reverse();     // Optional; call this to keep the order as written
-  System::log->printf("Open http://%s/timers in web browser\n", System::getLocalIPAddress().c_str());
+  System::log->printf("Open http://%s/timers in web browser and add some timers\n", System::getLocalIPAddress().c_str());
 }
 
 void loop() {
