83f33a67bf671a6591b6cf95999b7cf334b9409e
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2021-05-14 23:14:27 +0200

    Cosmetic fix

--- examples/rtcmem/MySystem.h
+++ examples/rtcmem/MySystem.h
@@ -1,6 +1,6 @@
 #ifndef _DS_SYSTEM_H_
 
-#define DS_CAP_SYS_RTCMEM   // enable RTC memory
+#define DS_CAP_SYS_RTCMEM   // Enable RTC memory
 #define DS_CAP_SYS_LOG_HW   // Enable syslog on hardware serial line
 
 #include "System.h"         // System global definitions
