cec086af6393f238c431e3717e654067ad5469c3
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2023-08-31 23:46:18 +0200

    Add the new macro DS_MAX_WEB_PAGE_SIZE to the web server example

--- examples/webserver/MySystem.h
+++ examples/webserver/MySystem.h
@@ -4,6 +4,10 @@
 #define DS_CAP_WEBSERVER    // Enable web server
 #define DS_CAP_SYS_LOG_HW   // Enable syslog on hardware serial line
 
+// Uncomment this if you want to customize the web page buffer size (default is 2 kiB pre-allocated in RAM)
+// Set this to 0 to avoid pre-allocation altogether and release memory after the page has been served
+//#define DS_MAX_WEB_PAGE_SIZE 2048U  // Web page buffer size
+
 #include "System.h"         // System global definitions
 
 #endif // _DS_SYSTEM_H_
